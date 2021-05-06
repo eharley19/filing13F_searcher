909b6ddcc11ac0b6fefefc0a3c73cc47722c9202
Rose <raltianas@gmail.com>
2021-05-06 00:26:10 -0400

    Update test_main.py with imports

--- python/tests/test_main.py
+++ python/tests/test_main.py
@@ -5,9 +5,17 @@ from edgar_filing_searcher.parsers.crawler_current_events import get_text, \
     parse_13f_filing_detail_urls
 from edgar_filing_searcher.parsers.parser_class import Parser
 from edgar_filing_searcher.parsers.setup_db_connection import setup_db_connection
+import pytest
 
 
-def test_create_url_list():
+@pytest.fixture
+def current_events_text():
+    """This function creates an fixture with test edgar_current_events html data"""
+    with open("fixtures/edgar_current_events.html", "rt") as file:
+        return file.read()
+
+
+def test_create_url_list(current_events_text):
     assert False
 
 
